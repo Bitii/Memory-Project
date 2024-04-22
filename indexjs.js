09b1e289cf8cf5c91ef03e82b11c395f23f51bde
Turcsan <52500215+Turcsan@users.noreply.github.com>
2024-04-22 18:15:12 +0200

    indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -6,14 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
         contentDiv.innerHTML = '';
         
         const images = [
-            "https://media.discordapp.net/attachments/1229720027574304900/1230136296609943612/hp_background.jpg?ex=663238b3&is=661fc3b3&hm=88658657964911388edcbf5c48fe67a356aae75cce7790c37e10a662adc0afdc&=&format=webp&width=824&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1229720946731122709/hatter.jpg?ex=6630b5e0&is=661e40e0&hm=04af5f7df764d2da72dfaf8a6de6eb2769af41e23f6754a67039e414f45ff3d9&=&format=webp&width=824&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230141863676350544/lamborghini.jpg?ex=66323de3&is=661fc8e3&hm=484df676945abfbbd3a0f06fb786a9744e6435dd1ca639ff04a424d8c2763262&=&format=webp&width=824&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230137892425437265/IMG_3762.jpg?ex=66323a30&is=661fc530&hm=36e51e4b724e0b2795609dc9ce84ce46425bf60e2bba1a47ad6c119580d41d85&=&format=webp&width=824&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230139204890005564/Lord_Of_the_rings.jpg?ex=66323b69&is=661fc669&hm=737596e29984a86df31a953639727ba831dd199ad5050bbba45f79ab67c99258&=&format=webp&width=961&height=405",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230140853712850974/disney.jpg?ex=66323cf2&is=661fc7f2&hm=4faf002e4b5e54ebd5898173ed925c736660b1cb483ddfdcab0b2f233139f2cf&=&format=webp&width=742&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230144635284881478/alcohol.jpg?ex=66324078&is=661fcb78&hm=08bdfc90c02af91358d8f3bf916d598f6bbf123824708d7ddb4ad6ee35ac0d22&=&format=webp&width=696&height=464",
-            "https://media.discordapp.net/attachments/1229720027574304900/1230149967218344017/lion.jpg?ex=6632456f&is=661fd06f&hm=8089eebcb9678e254c11a09fe23b779c1ca34b26cfd5860301ef4e622fb9fb39&=&format=webp&width=824&height=464"
+            //hp
+            "https://lh3.googleusercontent.com/pw/AP1GczO6Kw05h6y21acd-9SQSN5CZ5bUp28W2_8p8CE85h_yCqJeOHYJLhaEFPXbl9qdvgAo3LQZncWGb8_ePZALdON1o-sncafIlOBhxWh5nM-JE2OBy4RttQ_-4vE3ecm_dgH5vHfu9QWlF404qJkIRuI=w1080-h607-s-no-gm?authuser=0",
+            //got
+            "https://lh3.googleusercontent.com/pw/AP1GczNu-htEEAcqjIDSZsIMgVcgkWzW2H4opLedO7NikTIQd5v75VFjuvGM3HA7fXvjY0P7adSiY9yFClLeJG4c2Bt0CaggomXJEirrft9PuOQi2qfpIcciMnjbXIH9HfOsnv8BpMAzPszRRS7NDK4jpG0=w1639-h922-s-no-gm?authuser=0",
+            //kocsi
+            "https://lh3.googleusercontent.com/pw/AP1GczPeumNTkHCrPbASSfd3CoqRn_P8ZAXVoZPjM8pVtCYFT7jL-E838q3i-kksTZbMme5uTZbLOIdlYVSFr0IYLAv6nbmJU36LPPgay9dHBMoRkR0GeiWgvQF2QnWsutGHWuZJTqgqP-s0P3NUN7cr7GY=w1639-h922-s-no-gm?authuser=0",
+            //sw
+            "https://lh3.googleusercontent.com/pw/AP1GczNequYJOIlLRGCAWDNZQrEOcpVRMzqigE9fS0EsuYISxkD-nWXw8LylUEA3Rxdi2LUddXIEKO8uqCoQs6_PneNgo4szKiqWpF4U7Dls3c0MPXBIEvSY8SJrY4QenbGy-9OBgi4-lAu8BK8qkXdAyRQ=w1639-h922-s-no-gm?authuser=0",
+            //gyuruk
+            "https://lh3.googleusercontent.com/pw/AP1GczO0fmMue50hrYUnOz3laSXkw6GJgNTvMZFAD5_2D_GJEgl7QcrgemB4xLD0iHMczjzl8WhdvtdpDAwsAE4D6GuY4Tf0w7zIslEotyNlJqKO2mbbKkynHwvgZAoEKo3C6vnB4xRbTI3HCWDPbjELRk4=w2185-h922-s-no-gm?authuser=0",
+            //mesek
+            "https://lh3.googleusercontent.com/pw/AP1GczODmERn-4GiyBmgfLFfNJxneiHnSPfUtMr2E-blLoktS0JbGUWUnA9C2as4SioXOGVdDOioGxgd5bSo_QW8yjtJOAcmVyvAOE0dshM--Iv5BGQXJLtSmnq4tRiV3cp5W185YNlIESB3TOD6xU7xs88=w1475-h922-s-no-gm?authuser=0",
+            //piak
+            "https://lh3.googleusercontent.com/pw/AP1GczObXfDoPoSONoIyLkELq8m4Wo9wVWIaREqGeI3vi1V3OTmPv4aThBxNK9qKWy4ZgfrVHMVZOqZS09bY3Bucs9bjUdJG3G45MaPyqMQa8Ov1QRw4RDKh_s3iBxbGA7xW4mss1to0WLceaGDHd5ihypk=w1383-h922-s-no-gm?authuser=0",
+            //allatok
+            "https://lh3.googleusercontent.com/pw/AP1GczNsZe1G2ejZkSNTCaW2zgDGIXNjXY858Hq15TQsbUHV_v7-L7qeA8ELfa0UzGm6MBE4cVI-TOGOrgp86uuiOGB70kT7GqZFiIO7M7F8ehrSQaVWD4Md3bho4qvYGYdt1BGt6xg-fRLeX3ioSNPJP_I=w1639-h922-s-no-gm?authuser=0"
         ];
     
         const imageTrackDiv = document.getElementById('image-track');
