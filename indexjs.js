835be0c3e7f5847cfba5c80ddf314763a502ee03
Bitii <tari.tibor70@gmail.com>
2024-04-25 00:49:58 +0200

    html összefűzések

--- indexjs.js
+++ indexjs.js
@@ -1,54 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const borderChoice = document.getElementById("boardChoice");
-    const contentDiv = document.querySelector(".content");
+document.addEventListener("DOMContentLoaded", function () {
+  const borderChoice = document.getElementById("boardChoice");
+  const contentDiv = document.querySelector(".content");
 
-    document.getElementById('boardChoice').addEventListener('click', function() {
-        contentDiv.innerHTML = '';
-        
-        const images = [
-            //hp
-            "https://lh3.googleusercontent.com/pw/AP1GczO6Kw05h6y21acd-9SQSN5CZ5bUp28W2_8p8CE85h_yCqJeOHYJLhaEFPXbl9qdvgAo3LQZncWGb8_ePZALdON1o-sncafIlOBhxWh5nM-JE2OBy4RttQ_-4vE3ecm_dgH5vHfu9QWlF404qJkIRuI=w1080-h607-s-no-gm?authuser=0",
-            //got
-            "https://lh3.googleusercontent.com/pw/AP1GczNu-htEEAcqjIDSZsIMgVcgkWzW2H4opLedO7NikTIQd5v75VFjuvGM3HA7fXvjY0P7adSiY9yFClLeJG4c2Bt0CaggomXJEirrft9PuOQi2qfpIcciMnjbXIH9HfOsnv8BpMAzPszRRS7NDK4jpG0=w1639-h922-s-no-gm?authuser=0",
-            //kocsi
-            "https://lh3.googleusercontent.com/pw/AP1GczPeumNTkHCrPbASSfd3CoqRn_P8ZAXVoZPjM8pVtCYFT7jL-E838q3i-kksTZbMme5uTZbLOIdlYVSFr0IYLAv6nbmJU36LPPgay9dHBMoRkR0GeiWgvQF2QnWsutGHWuZJTqgqP-s0P3NUN7cr7GY=w1639-h922-s-no-gm?authuser=0",
-            //sw
-            "https://lh3.googleusercontent.com/pw/AP1GczNequYJOIlLRGCAWDNZQrEOcpVRMzqigE9fS0EsuYISxkD-nWXw8LylUEA3Rxdi2LUddXIEKO8uqCoQs6_PneNgo4szKiqWpF4U7Dls3c0MPXBIEvSY8SJrY4QenbGy-9OBgi4-lAu8BK8qkXdAyRQ=w1639-h922-s-no-gm?authuser=0",
-            //gyuruk
-            "https://lh3.googleusercontent.com/pw/AP1GczO0fmMue50hrYUnOz3laSXkw6GJgNTvMZFAD5_2D_GJEgl7QcrgemB4xLD0iHMczjzl8WhdvtdpDAwsAE4D6GuY4Tf0w7zIslEotyNlJqKO2mbbKkynHwvgZAoEKo3C6vnB4xRbTI3HCWDPbjELRk4=w2185-h922-s-no-gm?authuser=0",
-            //mesek
-            "https://lh3.googleusercontent.com/pw/AP1GczODmERn-4GiyBmgfLFfNJxneiHnSPfUtMr2E-blLoktS0JbGUWUnA9C2as4SioXOGVdDOioGxgd5bSo_QW8yjtJOAcmVyvAOE0dshM--Iv5BGQXJLtSmnq4tRiV3cp5W185YNlIESB3TOD6xU7xs88=w1475-h922-s-no-gm?authuser=0",
-            //piak
-            "https://lh3.googleusercontent.com/pw/AP1GczObXfDoPoSONoIyLkELq8m4Wo9wVWIaREqGeI3vi1V3OTmPv4aThBxNK9qKWy4ZgfrVHMVZOqZS09bY3Bucs9bjUdJG3G45MaPyqMQa8Ov1QRw4RDKh_s3iBxbGA7xW4mss1to0WLceaGDHd5ihypk=w1383-h922-s-no-gm?authuser=0",
-            //allatok
-            "https://lh3.googleusercontent.com/pw/AP1GczNsZe1G2ejZkSNTCaW2zgDGIXNjXY858Hq15TQsbUHV_v7-L7qeA8ELfa0UzGm6MBE4cVI-TOGOrgp86uuiOGB70kT7GqZFiIO7M7F8ehrSQaVWD4Md3bho4qvYGYdt1BGt6xg-fRLeX3ioSNPJP_I=w1639-h922-s-no-gm?authuser=0"
-        ];
-    
-        const imageTrackDiv = document.getElementById('image-track');
+  document.getElementById("boardChoice").addEventListener("click", function () {
+    contentDiv.innerHTML = "";
 
-        images.forEach((imageUrl, index) => {
-            const container = document.createElement('div');
-            container.classList.add('container');
-            container.id = `game${index + 1}`;
+    const images = [
+      //hp
+      "https://lh3.googleusercontent.com/pw/AP1GczO6Kw05h6y21acd-9SQSN5CZ5bUp28W2_8p8CE85h_yCqJeOHYJLhaEFPXbl9qdvgAo3LQZncWGb8_ePZALdON1o-sncafIlOBhxWh5nM-JE2OBy4RttQ_-4vE3ecm_dgH5vHfu9QWlF404qJkIRuI=w1080-h607-s-no-gm?authuser=0",
+      //got
+      "https://lh3.googleusercontent.com/pw/AP1GczNu-htEEAcqjIDSZsIMgVcgkWzW2H4opLedO7NikTIQd5v75VFjuvGM3HA7fXvjY0P7adSiY9yFClLeJG4c2Bt0CaggomXJEirrft9PuOQi2qfpIcciMnjbXIH9HfOsnv8BpMAzPszRRS7NDK4jpG0=w1639-h922-s-no-gm?authuser=0",
+      //kocsi
+      "https://lh3.googleusercontent.com/pw/AP1GczPeumNTkHCrPbASSfd3CoqRn_P8ZAXVoZPjM8pVtCYFT7jL-E838q3i-kksTZbMme5uTZbLOIdlYVSFr0IYLAv6nbmJU36LPPgay9dHBMoRkR0GeiWgvQF2QnWsutGHWuZJTqgqP-s0P3NUN7cr7GY=w1639-h922-s-no-gm?authuser=0",
+      //sw
+      "https://lh3.googleusercontent.com/pw/AP1GczNequYJOIlLRGCAWDNZQrEOcpVRMzqigE9fS0EsuYISxkD-nWXw8LylUEA3Rxdi2LUddXIEKO8uqCoQs6_PneNgo4szKiqWpF4U7Dls3c0MPXBIEvSY8SJrY4QenbGy-9OBgi4-lAu8BK8qkXdAyRQ=w1639-h922-s-no-gm?authuser=0",
+      //gyuruk
+      "https://lh3.googleusercontent.com/pw/AP1GczO0fmMue50hrYUnOz3laSXkw6GJgNTvMZFAD5_2D_GJEgl7QcrgemB4xLD0iHMczjzl8WhdvtdpDAwsAE4D6GuY4Tf0w7zIslEotyNlJqKO2mbbKkynHwvgZAoEKo3C6vnB4xRbTI3HCWDPbjELRk4=w2185-h922-s-no-gm?authuser=0",
+      //mesek
+      "https://lh3.googleusercontent.com/pw/AP1GczODmERn-4GiyBmgfLFfNJxneiHnSPfUtMr2E-blLoktS0JbGUWUnA9C2as4SioXOGVdDOioGxgd5bSo_QW8yjtJOAcmVyvAOE0dshM--Iv5BGQXJLtSmnq4tRiV3cp5W185YNlIESB3TOD6xU7xs88=w1475-h922-s-no-gm?authuser=0",
+      //piak
+      "https://lh3.googleusercontent.com/pw/AP1GczObXfDoPoSONoIyLkELq8m4Wo9wVWIaREqGeI3vi1V3OTmPv4aThBxNK9qKWy4ZgfrVHMVZOqZS09bY3Bucs9bjUdJG3G45MaPyqMQa8Ov1QRw4RDKh_s3iBxbGA7xW4mss1to0WLceaGDHd5ihypk=w1383-h922-s-no-gm?authuser=0",
+      //allatok
+      "https://lh3.googleusercontent.com/pw/AP1GczNsZe1G2ejZkSNTCaW2zgDGIXNjXY858Hq15TQsbUHV_v7-L7qeA8ELfa0UzGm6MBE4cVI-TOGOrgp86uuiOGB70kT7GqZFiIO7M7F8ehrSQaVWD4Md3bho4qvYGYdt1BGt6xg-fRLeX3ioSNPJP_I=w1639-h922-s-no-gm?authuser=0",
+    ];
 
-            const img = document.createElement('img');
-            img.classList.add('image');
-            img.src = imageUrl;
-            img.draggable = false;
+    const imageTrackDiv = document.getElementById("image-track");
 
-            container.appendChild(img);
-            imageTrackDiv.appendChild(container);
-        });
+    images.forEach((imageUrl, index) => {
+      const container = document.createElement("div");
+      container.classList.add("container");
+      container.id = `game${index + 1}`;
 
-        const newStyleSheet = document.createElement('link');
-        newStyleSheet.rel = 'stylesheet';
-        newStyleSheet.href = 'palyak/boardchoicestyle.css';
-    
-        const originalStyleSheet = document.querySelector('link[href="indexstyle.css"]');
-        originalStyleSheet.remove();
-    
-        document.head.appendChild(newStyleSheet);
-       
-        initBoardChoice();
+      const img = document.createElement("img");
+      img.classList.add("image");
+      img.src = imageUrl;
+      img.draggable = false;
+
+      container.appendChild(img);
+      imageTrackDiv.appendChild(container);
     });
+
+    const newStyleSheet = document.createElement("link");
+    newStyleSheet.rel = "stylesheet";
+    newStyleSheet.href = "palyak/boardchoicestyle.css";
+
+    const originalStyleSheet = document.querySelector(
+      'link[href="indexstyle.css"]'
+    );
+    originalStyleSheet.remove();
+
+    document.head.appendChild(newStyleSheet);
+
+    initBoardChoice();
+  });
 });
--- palyak/boardchoice.js
+++ palyak/boardchoice.js
@@ -1,96 +1,107 @@
 function initBoardChoice() {
-const track = document.getElementById("image-track");
+  const track = document.getElementById("image-track");
 
-
-// A csúszka elmozdítása a görgetés irányának megfelelően
-function handleScroll(event) {
+  // A csúszka elmozdítása a görgetés irányának megfelelően
+  function handleScroll(event) {
     const scrollAmount = event.deltaY || -event.detail; // Az egér görgetés mértéke különböző böngészőkben
     const currentPercentage = parseFloat(track.dataset.percentage) || 0;
     const maxDelta = window.innerWidth / 2;
     const maxPercentageChange = (scrollAmount / maxDelta) * -100;
     const nextPercentageUnconstrained = currentPercentage + maxPercentageChange;
-    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
+    const nextPercentage = Math.max(
+      Math.min(nextPercentageUnconstrained, 0),
+      -100
+    );
+
     track.dataset.percentage = nextPercentage;
-  
+
     track.style.transition = "transform 0.5s ease"; // Animáció sebességének beállítása (0.5s)
     track.style.transform = `translate(${nextPercentage}%, -50%)`;
-  
-    for(const image of track.getElementsByClassName("image")) {
-        image.style.transition = "object-position 0.5s ease"; // Animáció sebességének beállítása (0.5s)
-        image.style.objectPosition = `${100 + nextPercentage}% center`;
+
+    for (const image of track.getElementsByClassName("image")) {
+      image.style.transition = "object-position 0.5s ease"; // Animáció sebességének beállítása (0.5s)
+      image.style.objectPosition = `${100 + nextPercentage}% center`;
     }
-}
-
-// Görgetés esemény figyelése
-window.addEventListener('wheel', handleScroll);
-
-
-const createButton = () => {
-  const button = document.createElement("button");
-  button.textContent = "Játék indítása";
-  button.classList.add("btn");
-  document.body.appendChild(button);
-  return button;
-}
-
-const removeButton = (button) => {
-  if (button && button.parentNode) {
-    button.parentNode.removeChild(button);
   }
-}
 
-const toggleButtonPosition = (button) => {
-  button.style.position = "fixed";
-  button.style.left = "50%";
-  button.style.top = "50%";
-  button.style.transform = "translate(-50%, -50%)";
-}
+  // Görgetés esemény figyelése
+  window.addEventListener("wheel", handleScroll);
 
-const isClick=false;
+  var button;
+  const createButton = () => {
+    // Összes div elem megfogása "container" osztállyal
+    const divs = document.querySelectorAll(".container");
+    // Bejárja a div elemeket
+    divs.forEach((div) => {
+      // Hozzáad egy click eseményfigyelőt minden div-hez
+      div.addEventListener("click", () => {
+        // Létrehoz egy gombot, amikor a div-re kattintanak
+        button = document.createElement("button");
+        button.textContent = "Játék indítása";
+        button.classList.add("btn");
+        button.id = div.id; // Egyedi azonosítót rendel a gombhoz
+        document.body.appendChild(button);
+        // A gombhoz eseményfigyelő hozzáadása
+        button.addEventListener("click", () => {
+          window.location.href = `../memoriajatek[got]/${div.id}.html`;
+        });
+      });
+    });
+    return button;
+  };
 
-const toggleFullScreen = (e) => {
-  const {
-    top,
-    left,
-    width,
-    height
-  } = e.target.getBoundingClientRect();
+  const removeButton = (button) => {
+    if (button && button.parentNode) {
+      button.parentNode.removeChild(button);
+    }
+  };
 
-  let fullScreen = e.currentTarget.cloneNode(true);
-  fullScreen.style.setProperty("--inset", `${top}px auto auto ${left}px`);
-  fullScreen.style.width = `${width}px`;
-  fullScreen.style.height = `${height}px`;
-  fullScreen.style.objectPosition = 'center';
-  fullScreen.classList.add("full-screen");
-  fullScreen.addEventListener("click", shrink);
-  container.appendChild(fullScreen);
+  // const toggleButtonPosition = (button) => {
+  //   button.style.position = "fixed";
+  //   button.style.left = "50%";
+  //   button.style.top = "50%";
+  //   button.style.transform = "translate(-50%, -50%)";
+  // };
 
-  const button = createButton();
-  toggleButtonPosition(button);
-  isClick=!isClick
-}
+  const isClick = false;
 
-const shrink = (e) => {
-  const el = e.target;
-  el.addEventListener("animationend", (e) => e.target.remove());
-  el.style.animation = 'none';
-  el.offsetHeight;
-  el.style.animation = '';
-  el.classList.add("shrink-down");
-  
-  const button = document.querySelector(".btn");
-  if (button) {
-    removeButton(button); // A gomb eltávolítása
-  }
-}
+  const toggleFullScreen = (e) => {
+    const { top, left, width, height } = e.target.getBoundingClientRect();
 
-const boxes = document.querySelectorAll(".image");
-const container = document.body;
+    let fullScreen = e.currentTarget.cloneNode(true);
+    fullScreen.style.setProperty("--inset", `${top}px auto auto ${left}px`);
+    fullScreen.style.width = `${width}px`;
+    fullScreen.style.height = `${height}px`;
+    fullScreen.style.objectPosition = "center";
+    fullScreen.classList.add("full-screen");
+    fullScreen.addEventListener("click", shrink);
+    container.appendChild(fullScreen);
 
-boxes.forEach(box => {
-  box.addEventListener("click", toggleFullScreen);
-});
+    const button = createButton();
+    // toggleButtonPosition(button);
+    isClick = !isClick;
+  };
 
-const images = document.querySelectorAll('.image');
+  const shrink = (e) => {
+    const el = e.target;
+    el.addEventListener("animationend", (e) => e.target.remove());
+    el.style.animation = "none";
+    el.offsetHeight;
+    el.style.animation = "";
+    el.classList.add("shrink-down");
+
+    const button = document.querySelector(".btn");
+    if (button) {
+      removeButton(button); // A gomb eltávolítása
+    }
+  };
+
+  const boxes = document.querySelectorAll(".image");
+  const container = document.body;
+
+  boxes.forEach((box) => {
+    box.addEventListener("click", toggleFullScreen);
+  });
+
+  const images = document.querySelectorAll(".image");
 }
